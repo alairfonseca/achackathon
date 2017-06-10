81e1056db25e118a378eb8a4e20db103469759dd
Rennan <rennanpossas@gmail.com>
2017-06-10 14:27:49 -0300

    LoginController

--- src/main/java/com/hackathon/scalitimes/web/ControllerBase.java
+++ src/main/java/com/hackathon/scalitimes/web/ControllerBase.java
@@ -5,6 +5,9 @@ import java.io.UnsupportedEncodingException;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.hackathon.scalitimes.util.Constants;
 
 public class ControllerBase {
@@ -25,6 +28,42 @@ public class ControllerBase {
 		}
 		
 		return token;
+	}
+	
+	
+	protected <T> T decipherToken(String token, String claimKey, Class<T> type){
+		
+		DecodedJWT decodedJwt = null;
+		
+		try {
+		    decodedJwt = JWT.decode(token);
+		} catch (JWTDecodeException exception){
+		    //Invalid token
+		}
+		
+		//decodedJwt.
+		
+		Claim claim = decodedJwt.getClaim(claimKey);
+		
+		T objReturn = claim.as(type);
+		
+		/*String token = "";
+		
+		try {
+		    Algorithm algorithm = Algorithm.HMAC256(Constants.Keys.SECRET_KEY);
+		    token = JWT.create()
+		        .withIssuer(Constants.Keys.ISSUER_KEY)
+		        .withClaim(Constants.Keys.PROFILE_ID_KEY, profileId)
+		        .sign(algorithm);
+		} catch (UnsupportedEncodingException exception){
+		    //UTF-8 encoding not supported
+		} catch (JWTCreationException exception){
+		    //Invalid Signing configuration / Couldn't convert Claims.
+		}*/
+		
+		return objReturn;
+		
+		//return token;
 	}
 
 }
--- src/main/java/com/hackathon/scalitimes/web/LoginController.java
+++ src/main/java/com/hackathon/scalitimes/web/LoginController.java
@@ -4,13 +4,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hackathon.scalitimes.util.Constants;
+
 
 @RestController
+@RequestMapping(Constants.Config)
 public class LoginController extends ControllerBase {
 	@RequestMapping("/auth")
 	public String auth(@RequestParam (value="user") String user, @RequestParam (value="password") String password) {
 		
 		return generateToken(user, 1);
 		
+	}
+	
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>> listar() {
+		List<User> users = userService.listar();
+	
+		return ResponseEntity.status(HttpStatus.OK).body(users);
 	}
 }
