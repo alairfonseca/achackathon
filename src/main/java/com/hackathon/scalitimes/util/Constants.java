c13ae67c8ee2b07b82e68b9cc3a82cbc7ae0649a
Rennan <rennanpossas@gmail.com>
2017-06-10 12:47:56 -0300

    Autenticação usando JWT

---
+++ src/main/java/com/hackathon/scalitimes/util/Constants.java
@@ -0,0 +1,19 @@
+package com.hackathon.scalitimes.util;
+
+public class Constants {
+	
+	public static class Keys{
+		
+		public static String SECRET_KEY = "h4ck4thon";
+		public static String PROFILE_ID_KEY = "profileId";
+		public static String ISSUER_KEY = "Time1";
+		
+	}
+	
+	public static class Config{
+		
+		public static int KEY_EXPIRATION_TIME = 10;
+		
+	}
+
+}
--- src/main/java/com/hackathon/scalitimes/web/ControllerBase.java
+++ src/main/java/com/hackathon/scalitimes/web/ControllerBase.java
@@ -1,5 +1,30 @@
 package com.hackathon.scalitimes.web;
 
+import java.io.UnsupportedEncodingException;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.hackathon.scalitimes.util.Constants;
+
 public class ControllerBase {
+	
+	protected String generateToken(String userName, int profileId){
+		String token = "";
+		
+		try {
+		    Algorithm algorithm = Algorithm.HMAC256(Constants.Keys.SECRET_KEY);
+		    token = JWT.create()
+		        .withIssuer(Constants.Keys.ISSUER_KEY)
+		        .withClaim(Constants.Keys.PROFILE_ID_KEY, profileId)
+		        .sign(algorithm);
+		} catch (UnsupportedEncodingException exception){
+		    //UTF-8 encoding not supported
+		} catch (JWTCreationException exception){
+		    //Invalid Signing configuration / Couldn't convert Claims.
+		}
+		
+		return token;
+	}
 
 }
