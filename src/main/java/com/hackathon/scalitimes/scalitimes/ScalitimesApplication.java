51be15e0b1e02aa318c574cedff3d12a76e90834
Pedro <pedrogalvaosilva@gmail.com>
2017-06-10 10:39:28 -0300

    Project structure

---
+++ src/main/java/com/hackathon/scalitimes/scalitimes/ScalitimesApplication.java
@@ -0,0 +1,12 @@
+package com.hackathon.scalitimes.scalitimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalitimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalitimesApplication.class, args);
+	}
+}
