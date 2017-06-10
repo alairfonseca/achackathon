ab6eca73dde49080ef81519c8c8906d730c903fd
Rennan <rennanpossas@gmail.com>
2017-06-10 16:02:26 -0300

    Token

--- src/main/java/com/hackathon/scalitimes/web/LoginController.java
+++ src/main/java/com/hackathon/scalitimes/web/LoginController.java
@@ -3,6 +3,7 @@ package com.hackathon.scalitimes.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,7 @@ import com.hackathon.scalitimes.util.TokenUtil;
 
 @RestController
 @RequestMapping(Constants.Config.PATH_V1 + "auth")
+@CrossOrigin
 public class LoginController extends ControllerBase {
 	
 	@Autowired
